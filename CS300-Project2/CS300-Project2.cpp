d7db81806d084c09bcdd3cc01f5a573332bcb685
synsy <57045115+synsy@users.noreply.github.com>
2025-02-20 10:48:07 -0800

    Main Menu, Struct, HashTable skeleton
    Implemented a switch statement for the main menu options. Defined Course struct. Defined Nodes. Defined HashTable skeleton.

--- CS300-Project2/CS300-Project2.cpp
+++ CS300-Project2/CS300-Project2.cpp
@@ -1,20 +1,148 @@
-// CS300-Project2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// CS300-Project2.cpp 
+// Christopher O'Dell
+// CS300 - Southern New Hampshire University
+// Project 2
 //
 
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+const unsigned int DEFAULT_SIZE = 50;
+
+// Course Structure
+struct Course
+{
+	string courseNumber;
+	string courseName;
+	vector<string> coursePrerequisites;
+};
+
+class HashTable {
+
+private:
+	struct Node {
+		Course course;
+		unsigned int key;
+		Node* next;
+
+		Node() {
+			key = UINT_MAX;
+			next = nullptr;
+		}
+
+		Node(Course aCourse) : Node() {
+			course = aCourse;
+		}
+
+		Node(Course aCourse, unsigned int aKey) : Node(aCourse) {
+			key = aKey;
+		}
+	};
+
+	vector<Node> nodes;
+	unsigned int tableSize = DEFAULT_SIZE;
+	unsigned int hash(int key);
+
+public:
+	HashTable();
+	HashTable(unsigned int size);
+	virtual ~HashTable();
+	void PrintAll();
+	void PrintCourse(string courseNumber);
+};
+
+/**
+ * Default constructor
+ */
+HashTable::HashTable() {
+	// Initialize the structures used to hold courses
+	// Initalize node structure by resizing tableSize
+	nodes.resize(tableSize);
+}
+
+/**
+ * Constructor for specifying size of the table
+ * Use to improve efficiency of hashing algorithm
+ * by reducing collisions without wasting memory.
+ */
+HashTable::HashTable(unsigned int size) {
+	// invoke local tableSize to size with this->
+	this->tableSize = size;
+	// resize nodes size
+	nodes.resize(tableSize);
+}
+
+/**
+ * Destructor
+ */
+HashTable::~HashTable() {
+	// Logic to free storage when class is destroyed
+	nodes.erase(nodes.begin());
+	// erase nodes beginning
+}
+
+/**
+ * Calculate the hash value of a given key.
+ * Note that key is specifically defined as
+ * unsigned int to prevent undefined results
+ * of a negative list index.
+ *
+ * @param key The key to hash
+ * @return The calculated hash
+ */
+unsigned int HashTable::hash(int key) {
+	// Logic to calculate a hash value
+	// return key tableSize
+	return key % tableSize;
+}
+
+/**
+ * Print all bids
+ */
+void HashTable::PrintAll() {
+	//FIXME: Implement this function
+}
+
+/**
+* Print a specific course
+*/
+void HashTable::PrintCourse(string courseNumber) {
+	//FIXME: Implement this function
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	cout << "Welcome to the Course Planner!" << endl;
+
+    int choice = 0;
+    while (choice != 9)
+    {
+        cout << "1. Load Data Structure." << endl;
+		cout << "2. Print Course List." << endl;
+		cout << "3. Print Course." << endl;
+		cout << "9. Exit" << endl;
+		cout << "Enter your choice: ";
+		cin >> choice;
+
+		switch (choice)
+		{
+		case 1:
+			cout << "Load Course Data" << endl;
+			break;
+		case 2:
+			cout << "Print Course List" << endl;
+			break;
+		case 3:
+			cout << "Print Course" << endl;
+			break;
+		case 9:
+			cout << "Thank you for using the course planner!" << endl;
+			break;
+		default:
+			cout << choice << " is not a valid option." << endl;
+			break;
+		}
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
